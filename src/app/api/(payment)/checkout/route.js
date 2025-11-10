b25d1ffbbf878098be3a20a7b4c16e4327242970
Pranav <pranavmolawade123@gmail.com>
2025-11-10 20:34:09 +0530

    new key added

--- src/app/api/(payment)/checkout/route.js
+++ src/app/api/(payment)/checkout/route.js
@@ -9,6 +9,7 @@ export async function POST(req) {
       key_id: process.env.RAZORPAY_KEY_ID,
       key_secret: process.env.RAZORPAY_KEY_SECRET,
     });
+    console.log(razorpay)
 
     const options = {
       amount: Math.round(amount * 100), // amount in paise
